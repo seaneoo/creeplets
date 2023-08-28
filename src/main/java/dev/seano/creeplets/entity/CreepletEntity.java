13d6499b3cb1adc73b1c09e85234940a746ccbc1
Sean O'Connor <so@seano.dev>
2023-08-28 16:00:23 -0600

    Update attribute GENERIC_MOVEMENT_SPEED

--- src/main/java/dev/seano/creeplets/entity/CreepletEntity.java
+++ src/main/java/dev/seano/creeplets/entity/CreepletEntity.java
@@ -1,11 +1,18 @@
 package dev.seano.creeplets.entity;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.world.World;
 
 public class CreepletEntity extends CreeperEntity {
     public CreepletEntity(EntityType<? extends CreeperEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    public static DefaultAttributeContainer.Builder createCreeperAttributes() {
+        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.4);
     }
 }
