93c8b6ddb90ce34fecb150141909666fc29f69af
Sean O'Connor <so@seano.dev>
2023-08-28 12:09:15 -0600

    Initial commit

---
+++ src/client/java/dev/seano/creeplets/CreepletsModClient.java
@@ -0,0 +1,10 @@
+package dev.seano.creeplets;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreepletsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/dev/seano/creeplets/CreepletsMod.java
@@ -0,0 +1,15 @@
+package dev.seano.creeplets;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreepletsMod implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("creeplets");
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
