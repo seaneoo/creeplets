0b5fa0c105a5ac1a36d998f69cf84c37e7e01acb
Sean O'Connor <so@seano.dev>
2023-08-29 16:54:37 -0600

    Set health, fuse time, and explosion radius; modify movement speed

--- src/main/java/dev/seano/creeplets/entity/CreepletEntity.java
+++ src/main/java/dev/seano/creeplets/entity/CreepletEntity.java
@@ -1,5 +1,6 @@
 package dev.seano.creeplets.entity;
 
+import dev.seano.creeplets.mixin.CreeperEntityAccessor;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
@@ -7,18 +8,21 @@ import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.world.World;
 
-/**
- * TODO: - Change fuse speed to '15' (down from '30' as the default)
- *       - Make the explosion radius smaller (only slightly, original is '3')
- */
+@SuppressWarnings("FieldCanBeLocal")
 public class CreepletEntity extends CreeperEntity {
-    private static final float movementSpeed = 0.45f;
+    private static final float movementSpeed = 0.4f;
+    private static final float health = 10;
+    private final int fuseTime = 10;
+    private final int explosionRadius = 1;
 
     public CreepletEntity(EntityType<? extends CreeperEntity> entityType, World world) {
         super(entityType, world);
+
+        ((CreeperEntityAccessor) this).setFuseTime(this.fuseTime);
+        ((CreeperEntityAccessor) this).setExplosionRadius(this.explosionRadius);
     }
 
     public static DefaultAttributeContainer.Builder createCreeperAttributes() {
-        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MOVEMENT_SPEED, movementSpeed);
+        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MOVEMENT_SPEED, movementSpeed).add(EntityAttributes.GENERIC_MAX_HEALTH, health);
     }
 }
---
+++ src/main/java/dev/seano/creeplets/mixin/CreeperEntityAccessor.java
@@ -0,0 +1,14 @@
+package dev.seano.creeplets.mixin;
+
+import net.minecraft.entity.mob.CreeperEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(CreeperEntity.class)
+public interface CreeperEntityAccessor {
+    @Accessor("fuseTime")
+    void setFuseTime(int fuseTime);
+
+    @Accessor("explosionRadius")
+    void setExplosionRadius(int explosionRadius);
+}
