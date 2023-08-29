192992f183b3f63c3b6ca0329869e5ef304862ce
Sean O'Connor <so@seano.dev>
2023-08-28 19:01:56 -0600

    Update CreepletEntity

--- src/main/java/dev/seano/creeplets/entity/CreepletEntity.java
+++ src/main/java/dev/seano/creeplets/entity/CreepletEntity.java
@@ -7,12 +7,18 @@ import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.world.World;
 
+/**
+ * TODO: - Change fuse speed to '15' (down from '30' as the default)
+ *       - Make the explosion radius smaller (only slightly, original is '3')
+ */
 public class CreepletEntity extends CreeperEntity {
+    private static final float movementSpeed = 0.45f;
+
     public CreepletEntity(EntityType<? extends CreeperEntity> entityType, World world) {
         super(entityType, world);
     }
 
     public static DefaultAttributeContainer.Builder createCreeperAttributes() {
-        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.4);
+        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MOVEMENT_SPEED, movementSpeed);
     }
 }
