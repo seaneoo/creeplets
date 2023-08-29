997c2327b8742ea4d900120035afc8366af860af
Sean O'Connor <so@seano.dev>
2023-08-28 18:52:37 -0600

    Add spawn egg

--- src/main/java/dev/seano/creeplets/CreepletsMod.java
+++ src/main/java/dev/seano/creeplets/CreepletsMod.java
@@ -2,11 +2,14 @@ package dev.seano.creeplets;
 
 import dev.seano.creeplets.entity.CreepletEntity;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -25,6 +28,11 @@ public class CreepletsMod implements ModInitializer {
     @Override
     public void onInitialize() {
         FabricDefaultAttributeRegistry.register(Entities.CREEPLET, CreepletEntity.createCreeperAttributes());
+
+        Registry.register(Registries.ITEM, identifier("creeplet_spawn_egg"), Items.CREEPLET_SPAWN_EGG);
+
+        //noinspection UnstableApiUsage
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(entries -> entries.addAfter(net.minecraft.item.Items.CREEPER_SPAWN_EGG, Items.CREEPLET_SPAWN_EGG));
     }
 
     public static Identifier identifier(String path) {
@@ -33,5 +41,9 @@ public class CreepletsMod implements ModInitializer {
 
     static class Entities {
         public static final EntityType<CreepletEntity> CREEPLET = Registry.register(Registries.ENTITY_TYPE, identifier("creeplet"), FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, CreepletEntity::new).dimensions(EntityDimensions.fixed(0.3f, 0.85f)).build());
+    }
+
+    static class Items {
+        public static final Item CREEPLET_SPAWN_EGG = new SpawnEggItem(Entities.CREEPLET, 894731, 0, new FabricItemSettings());
     }
 }
