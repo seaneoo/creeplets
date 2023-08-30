219f2e2763e24cfac9b0afed2d5d2d66cf353ad7
Sean O'Connor <so@seano.dev>
2023-08-29 18:28:02 -0600

    Add natural spawning

--- src/main/java/dev/seano/creeplets/registry/ModEntities.java
+++ src/main/java/dev/seano/creeplets/registry/ModEntities.java
@@ -2,13 +2,18 @@ package dev.seano.creeplets.registry;
 
 import dev.seano.creeplets.CreepletsMod;
 import dev.seano.creeplets.entity.CreepletEntity;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.world.Heightmap;
 
 public class ModEntities {
     public static final EntityType<CreepletEntity> CREEPLET = Registry.register(Registries.ENTITY_TYPE, CreepletsMod.identifier("creeplet"), FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, CreepletEntity::new).dimensions(EntityDimensions.fixed(0.3f, 0.85f)).build());
@@ -19,6 +24,7 @@ public class ModEntities {
     }
 
     private static void registerSpawns() {
-        // TODO
+        SpawnRestriction.register(CREEPLET, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, HostileEntity::canSpawnInDark);
+        BiomeModifications.addSpawn(BiomeSelectors.spawnsOneOf(EntityType.CREEPER), SpawnGroup.MONSTER, CREEPLET, 100, 1, 2);
     }
 }
