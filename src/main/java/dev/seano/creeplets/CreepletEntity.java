978231f9827aa686c31721ffb2b4b80b5d5c9abf
Sean O'Connor <so@seano.dev>
2024-10-13 12:29:22 -0600

    Create CreeperEntityAccessor mixin, define fuse time & explosion radius

--- src/main/java/dev/seano/creeplets/CreepletEntity.java
+++ src/main/java/dev/seano/creeplets/CreepletEntity.java
@@ -1,5 +1,6 @@
 package dev.seano.creeplets;
 
+import dev.seano.creeplets.mixin.CreeperEntityAccessor;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
@@ -9,12 +10,19 @@ import net.minecraft.world.World;
 
 public class CreepletEntity extends CreeperEntity {
 
+	private static final int FUSE_TIME = 15;
+
+	private static final int EXPLOSION_RADIUS = 2;
+
 	private static final double MOVEMENT_SPEED = 0.4;
 
 	private static final double MAX_HEALTH = 10;
 
 	public CreepletEntity(EntityType<? extends CreeperEntity> entityType, World world) {
 		super(entityType, world);
+
+		((CreeperEntityAccessor) this).setFuseTime(FUSE_TIME);
+		((CreeperEntityAccessor) this).setExplosionRadius(EXPLOSION_RADIUS);
 	}
 
 	public static DefaultAttributeContainer.Builder createAttributes() {
